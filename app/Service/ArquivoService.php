b1bded3197b11ac18c611ccb4634e77a2d5b6a8d
JonnasWillian <jonnasnogueira2@gmail.com>
2026-03-27 22:38:47 -0300

    explicando melhor forma de uso para guardar arquivos

--- app/Service/ArquivoService.php
+++ app/Service/ArquivoService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 class ArquivoService {
     public function salveFile($arquivo){
+        // Salva o arquivo e retorna o caminho local, mas o indicado séria um serviço de storage para registro e backUp dos arquivos
         $fileLocal = $arquivo->store('arquivos', 'public');
 
         return $fileLocal;
