f518201a5f34d22411e360f29fe0d512408b52f4
JonnasWillian <jonnasnogueira2@gmail.com>
2026-04-16 22:12:54 -0300

    criando estrutura e relacao com a tabela de tag

---
+++ database/migrations/2026_04_14_014218_users_add_column_tags.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->unsignedBigInteger('tag_id')->after('user_id')->nullable();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->dropColumn('tag_id');
+        });
+    }
+};
